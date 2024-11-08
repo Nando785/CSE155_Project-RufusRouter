ea74b41110d21723c80b5bb73802c4a010fe2289
Dylan Baudhuin <dylanbaudhuinbarrett@gmail.com>
2024-11-08 15:04:30 -0800

    Map Data Implimented
    Implimented Complete Map Data and removed redundant map.  There is now a "clean" map and one with the edge/vector data in it (Prefixed with "Edited").

--- Project Map Algorithm/graph.py
+++ Project Map Algorithm/graph.py
@@ -32,43 +32,43 @@ graph = {
         {'to': 'se2', 'weight': 276.14, 'pathName': 'mineralKingBridge'}
     ],
     'NodeSE': [
-        {'to': 'cob1', 'weight': NULL, 'pathName': 'SE1WalkWay'},
-        {'to': 'se2', 'weight': NULL, 'pathName': 'mineralKingBridge2'},
-        {'to': 'bsp', 'weight': NULL, 'pathName': 'mineralKingBridge'}
+        {'to': 'cob1', 'weight': 490.52, 'pathName': 'SE1WalkWay'},
+        {'to': 'se2', 'weight': 165.31, 'pathName': 'mineralKingBridge2'},
+        {'to': 'bsp', 'weight': 700, 'pathName': 'mineralKingBridge'}
+    ],
+    'se2':[
+        {'to':'bsp', 'weight': 165.31, 'pathName':'mineralKingBridge2'},
+        {'to':'node3', 'weight': 474.24, 'pathName':'anselAdamsRoad1'}
+    ],
+    'node3':[
+        {'to':'se2', 'weight': 474.24, 'pathName':'anselAdamsRoad1'},
+        {'to':'ssm', 'weight': 209.69, 'pathName':'scholarsPlaza'},
+        {'to':'ssb', 'weight': 245.55, 'pathName':'anselAdamsRoad2'},
+        {'to':'cob1', 'weight': 165.84, 'pathName':'scholarsLane4'}
+    ],
+    'ssm':[
+        {'to':'node3', 'weight': 209.69, 'pathName':'scholarsPlaza'}
+    ],
+    'cob1':[
+        {'to':'node3', 'weight': 165.84, 'pathName':'scholarsLane4'},
+        {'to':'node2', 'weight': 220.18, 'pathName':'scholarsLane3'},
+        {'to':'NodeSE', 'weight': 490.52, 'pathName':'SE1WalkWay'}
+    ],
+    'node2':[
+        {'to':'library1', 'weight': 348, 'pathName':'scholarsLane2'},
+        {'to':'cob1', 'weight': 220.18, 'pathName':'scholarsLane3'},
+        {'to':'cob2', 'weight': 223.76, 'pathName':'ck'}
+    ],
+    'cob2':[
+        {'to':'library2', 'weight': 255.36, 'pathName':'socialJusticePlaza1'},
+        {'to':'node2', 'weight': 223.76, 'pathName':'ck'},
+        {'to':'ssb', 'weight': 385.39, 'pathName':'socialJusticePlaza2'}
+    ],
+    'library2':[
+        {'to':'cob2', 'weight': 255.36, 'pathName':'socialJusticePlaza1'}
+    ],
+    'ssb':[
+        {'to':'cob2', 'weight': 385.39, 'pathName':'socialJusticePlaza2'},
+        {'to':'node3', 'weight': 245.55, 'pathName':'anselAdamsRoad2'}
     ]
-    # 'se2':[
-    #     {'to':'bsp', 'weight': NULL, 'pathName':'mineralKingBridge2'},
-    #     {'to':'node3', 'weight': NULL, 'pathName':'anselAdamsRoad1'}
-    # ],
-    # 'node3':[
-    #     {'to':'se2', 'weight': NULL, 'pathName':'anselAdamsRoad1'},
-    #     {'to':'ssm', 'weight': NULL, 'pathName':'scholarsPlaza'},
-    #     {'to':'ssb', 'weight': NULL, 'pathName':'anselAdamsRoad2'},
-    #     {'to':'cob1', 'weight': NULL, 'pathName':'scholarsLane4'}
-    # ],
-    # 'ssm':[
-    #     {'to':'node3', 'weight': NULL, 'pathName':'scholarsPlaza'}
-    # ],
-    # 'cob1':[
-    #     {'to':'node3', 'weight': NULL, 'pathName':'scholarsLane4'},
-    #     {'to':'node2', 'weight': NULL, 'pathName':'scholarsLane3'}.
-    #     {'to':'NodeSE', 'weight': NULL, 'pathName':'SE1WalkWay'}
-    # ],
-    # 'node2':[
-    #     {'to':'library1', 'weight': NULL, 'pathName':'scholarsLane2'},
-    #     {'to':'cob1', 'weight': NULL, 'pathName':'scholarsLane3'},
-    #     {'to':'cob2', 'weight': NULL, 'pathName':'ck'}
-    # ],
-    # 'cob2':[
-    #     {'to':'library2', 'weight': NULL, 'pathName':'socialJusticePlaza1'},
-    #     {'to':'node2', 'weight': NULL, 'pathName':'ck'},
-    #     {'to':'ssb', 'weight': NULL, 'pathName':'socialJusticePlaza2'}
-    # ],
-    # 'library2':[
-    #     {'to':'cob2', 'weight': NULL, 'pathName':'socialJusticePlaza1'}
-    # ],
-    # 'ssb':[
-    #     {'to':'cob2', 'weight': NULL, 'pathName':'socialJusticePlaza2'},
-    #     {'to':'node3', 'weight': NULL, 'pathName':'anselAdamsRoad2'}
-    # ]
 }
