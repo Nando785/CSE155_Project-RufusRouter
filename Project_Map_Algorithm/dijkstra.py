81f649324109da13031601925857d1188b706bed
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2024-11-25 12:56:33 -0800

    Updated Dijkstra algorithm
    Changed input and output of algorithm

--- Project_Map_Algorithm/dijkstra.py
+++ Project_Map_Algorithm/dijkstra.py
@@ -3,14 +3,8 @@ import math
 
 # INPUT: 
 #   1) Source node/ position of user
-#   2) School map from graph
 
-# EX: Starting from SRE, find path to {COB2, Library, ACS}
-
-
-# Potential Issues:
-#   1) May need to recalculate path after a new node has been visited
-#       - Remove source and previously visited nodes from list
+# EX: Starting from SRE, find path to COB2
 
 
 # ===== Implement path finding algorithm here =====
@@ -21,11 +15,7 @@ class Graph():
         self.V = vertices
         self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
         self.vertex_map = {}
- 
-    def printSolution(self, dist, vertex_names):
-        print("Vertex \t Distance from Source")
-        for i in range(self.V):
-            print(vertex_names[i], "\t\t", dist[i])
+        self.edge_map = {}
  
     def minDistance(self, dist, sptSet):
         min = float('inf')
@@ -38,28 +28,38 @@ class Graph():
 
         return min_index
  
-    def dijkstra(self, src):
+    def dijkstra(self, start, end):
         dist = [float('inf')] * self.V
-        dist[src] = 0
+        dist[start] = 0
         sptSet = [False] * self.V
+        predecessors = [-1] * self.V
+        path_names = [None] * self.V
 
         for _ in range(self.V):
             u = self.minDistance(dist, sptSet)
             sptSet[u] = True
 
             for v in range(self.V):
-                if (self.graph[u][v] > 0 and not sptSet[v] and
-                   dist[v] > dist[u] + self.graph[u][v]):
+                if (self.graph[u][v] > 0 and not sptSet[v] and dist[v] > dist[u] + self.graph[u][v]):
                     dist[v] = dist[u] + self.graph[u][v]
-
-        self.printSolution(dist, list(self.vertex_map.keys()))
+                    predecessors[v] = u
+                    path_names[v] = self.edge_map[(u,v)]
+                    
+        path = []
+        current = end
+        while current  != -1 and predecessors[current] != -1:
+            path.insert(0, path_names[current])
+            current = predecessors[current]
+        return path
  
-    def add_edge(self, from_vertex, to_vertex, weight):
+    def add_edge(self, from_vertex, to_vertex, weight, path_name):
         if from_vertex in self.vertex_map and to_vertex in self.vertex_map:
             u = self.vertex_map[from_vertex]
             v = self.vertex_map[to_vertex]
             self.graph[u][v] = weight
             self.graph[v][u] = weight  # For undirected graph
+            self.edge_map[(u, v)] = path_name
+            self.edge_map[(v, u)] = path_name  # Both directions
 
     def setup_graph(self, graph):
         self.vertex_map = {node: index for index, node in enumerate(graph.keys())}
@@ -69,12 +69,24 @@ class Graph():
         for node, edges in graph.items():
             for edge in edges:
                 if edge['weight'] is not None:  # Ensure weight is not NULL
-                    self.add_edge(node, edge['to'], edge['weight'])
+                    self.add_edge(node, edge['to'], edge['weight'], edge['pathName'])
+    
+    def get_path_names(self, path):
+        reverse_map = {v: k for k, v in self.vertex_map.items()}
+        return [reverse_map[node] for node in path]
 
 # Driver program
 g = Graph(len(graph))
 g.setup_graph(graph)
-g.dijkstra(g.vertex_map['SRE'])
+
+startNode = 'SRE'
+endNode = 'cob2'
+startIndex = g.vertex_map[startNode]
+endIndex = g.vertex_map[endNode]
+
+pathNames = g.dijkstra(startIndex, endIndex)
+
+print("Shortest path: ", pathNames)
 
 
 # ===== Debugging: Print all graph nodes and their connections =====
